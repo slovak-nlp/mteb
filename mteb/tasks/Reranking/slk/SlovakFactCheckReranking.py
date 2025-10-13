c1ac25abcfb78017423c9f34d76d000f15d1929b
Andrej Ridzik <andrej.ridzik@kinit.sk>
2025-10-13 11:16:58 +0200

    Update task metadata

--- mteb/tasks/Reranking/slk/SlovakFactCheckReranking.py
+++ mteb/tasks/Reranking/slk/SlovakFactCheckReranking.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 
-from mteb.abstasks.TaskMetadata import TaskMetadata
 from mteb.abstasks.AbsTaskReranking import AbsTaskReranking
-
-
-_EVAL_SPLIT = "test"
+from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
 class SlovakFactCheckReranking(AbsTaskReranking):
@@ -23,29 +20,38 @@ class SlovakFactCheckReranking(AbsTaskReranking):
         type="Reranking",
         category="s2s",
         modalities=["text"],
-        eval_splits=[_EVAL_SPLIT],
+        eval_splits=["test"],
         eval_langs=["slk-Latn"],
         main_score="map",
         date=("2020-01-01", "2025-03-31"),
         domains=["News", "Social", "Written"],
         task_subtypes=["Article retrieval"],
         license="cc-by-nc-nd-4.0",
-        annotations_creators="derived",
+        annotations_creators="human-annotated",
         dialect=[],
-        sample_creation="found",
-        bibtex_citation=r""",
+        sample_creation="created",
+        bibtex_citation=r"""
 @inproceedings{pikuliak-etal-2023-multilingual,
-  title = "Multilingual Previously Fact-Checked Claim Retrieval",
-  author = "Pikuliak, Mat{\'u}{\v{s}} and Srba, Ivan and Moro, Robert and Hromadka, Timo and Smole{\v{n}}, Timotej and Meli{\v{s}}ek, Martin and Vykopal, Ivan and Simko, Jakub and Podrou{\v{z}}ek, Juraj and Bielikova, Maria",
-  editor = "Bouamor, Houda  and Pino, Juan  and Bali, Kalika",
-  booktitle = "Proceedings of the 2023 Conference on Empirical Methods in Natural Language Processing",
+  address = {Singapore},
+  author = {Pikuliak, Mat{\'u}{\v{s}}  and
+Srba, Ivan  and
+Moro, Robert  and
+Hromadka, Timo  and
+Smole{\v{n}}, Timotej  and
+Meli{\v{s}}ek, Martin  and
+Vykopal, Ivan  and
+Simko, Jakub  and
+Podrou{\v{z}}ek, Juraj  and
+Bielikova, Maria},
+  booktitle = {Proceedings of the 2023 Conference on Empirical Methods in Natural Language Processing},
+  doi = {10.18653/v1/2023.emnlp-main.1027},
+  editor = {Bouamor, Houda  and Pino, Juan  and Bali, Kalika},
   month = dec,
-  year = "2023",
-  address = "Singapore",
-  publisher = "Association for Computational Linguistics",
-  url = "https://aclanthology.org/2023.emnlp-main.1027",
-  doi = "10.18653/v1/2023.emnlp-main.1027",
-  pages = "16477--16500",
+  pages = {16477--16500},
+  publisher = {Association for Computational Linguistics},
+  title = {Multilingual Previously Fact-Checked Claim Retrieval},
+  url = {https://aclanthology.org/2023.emnlp-main.1027},
+  year = {2023},
 }
 """,
     )
