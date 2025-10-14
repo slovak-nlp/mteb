7ac948f9bcb08476d8208591a9f8e4d9a111de3a
Andrej Ridzik <andrej.ridzik@kinit.sk>
2025-10-14 10:54:04 +0200

    Update SlovakNLI

--- mteb/tasks/PairClassification/slk/SlovakNLI.py
+++ mteb/tasks/PairClassification/slk/SlovakNLI.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from datasets import DatasetDict
+
 from mteb.abstasks.AbsTaskPairClassification import AbsTaskPairClassification
 from mteb.abstasks.TaskMetadata import TaskMetadata
 
@@ -11,32 +13,34 @@ class SlovakNLI(AbsTaskPairClassification):
         reference="https://huggingface.co/datasets/natalia-nk/NLI-SK-annotated",
         dataset={
             "path": "natalia-nk/NLI-SK-annotated",
-            "revision": "220b9cfa730c36ec563517fcd749c9c7a5ae4436",
+            "revision": "79914f425b59d8b9fabb6d38c37f6d81f9723f46",
         },
         type="PairClassification",
         category="s2s",
         eval_splits=["test"],
         eval_langs=["slk-Latn"],
         main_score="max_ap",
+        license="not specified",
         annotations_creators="human-annotated",
     )
 
     def dataset_transform(self):
         _dataset = {}
 
-        # TODO: The dataset currently has only 'train' split
-        hf_dataset = self.dataset["train"].filter(
-            lambda x: x["Label"] in ["Entailment", "Contradiction"])
-        hf_dataset = hf_dataset.map(
-            lambda example: {"Label": 1 if example["Label"] == "Entailment" else 0}
-        )
+        for split in self.metadata.eval_splits:
+            hf_dataset = self.dataset[split].filter(
+                lambda x: x["Label"] in ["Entailment", "Contradiction"]
+            )
+            hf_dataset = hf_dataset.map(
+                lambda example: {"Label": 1 if example["Label"] == "Entailment" else 0}
+            )
 
-        _dataset["test"] = [
-            {
-                "sentence1": hf_dataset["Premise"],
-                "sentence2": hf_dataset["Hypothesis"],
-                "labels": hf_dataset["Label"],
-            }
-        ]
+            _dataset[split] = [
+                {
+                    "sentence1": hf_dataset["Premise"],
+                    "sentence2": hf_dataset["Hypothesis"],
+                    "labels": hf_dataset["Label"],
+                }
+            ]
 
-        self.dataset = _dataset
+        self.dataset = DatasetDict(_dataset)
