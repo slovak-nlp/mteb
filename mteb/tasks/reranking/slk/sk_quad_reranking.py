a3eebcf525353770d78fedd17354c744592337a8
Andrej Ridzik <andrej.ridzik@kinit.sk>
2025-12-12 13:49:41 +0100

    Update Slovak reranking tasks: revise dataset revisions, adjust `main_score`

--- mteb/tasks/reranking/slk/sk_quad_reranking.py
+++ mteb/tasks/reranking/slk/sk_quad_reranking.py
@@ -1,21 +1,21 @@
+from mteb.abstasks.retrieval import AbsTaskRetrieval
 from mteb.abstasks.task_metadata import TaskMetadata
-from mteb.abstasks.text.reranking import AbsTaskReranking
 
 
-class SkQuadReranking(AbsTaskReranking):
+class SkQuadReranking(AbsTaskRetrieval):
     metadata = TaskMetadata(
         name="SkQuadReranking",
         description=""" From Retrieval Sk QUAD """,
         reference="https://huggingface.co/datasets/TUKE-KEMT/reranking-skquad",
         dataset={
             "path": "TUKE-KEMT/reranking-skquad",
-            "revision": "7cfcb122006b33d150b485caac5aa41939b3ba37",
+            "revision": "3997eba1c60721e34f7b01db400f5b14a40218ea",
         },
         type="Reranking",
         category="t2t",
         eval_splits=["test"],
         eval_langs=["slk-Latn"],
-        main_score="map",
+        main_score="map_at_1000",
         date=("2025-10-09", "2025-10-09"),
         license="cc-by-sa-4.0",
         annotations_creators="derived",
